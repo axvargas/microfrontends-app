dd7194996f2555f5772756768efbb9a4c87677ee
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2022-02-07 22:07:35 -0500

    Added th webpack configs and container workflow

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,6 +1,5 @@
 
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common.js')
 const packageJson = require('../package.json')
@@ -30,10 +29,7 @@ const devConfig = {
       //     requiredVersion: '^17.0.2'
       //   },
       // }
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
+    })
   ],
 };
 
