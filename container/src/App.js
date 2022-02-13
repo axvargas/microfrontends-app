40d0ceadac3d04901a15e0f200033dba106d2e30
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2022-02-13 02:52:00 -0500

    [Container] Added  CssBaseline

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
+import { CssBaseline } from '@mui/material'
 const App = () => {
   return (
     <BrowserRouter>
+      <CssBaseline/>
       <Header />
       <MarketingApp />
     </BrowserRouter>
