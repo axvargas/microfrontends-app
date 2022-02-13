458d2b4da1e8997168ed8d63070f2febd2745987
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2022-02-13 02:29:42 -0500

    [Container] Added the header

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,12 @@
+import Header from './components/Header'
+import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
 const App = () => {
   return (
-    <>
-      <div>Hey there from container app!</div>
-      <p>Change from container dev intern</p>
-      <br />
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </>
+    </BrowserRouter>
   )
 }
 
